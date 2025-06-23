d53726e518879209bed2b39f1b4802d3ddc49a79
Antonio Peña Peña <antoniopenapena52@gmail.com>
2025-06-23 22:37:15 +0100

    Adding to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Obs } from "./obs";
+export { Get } from "./cache";
 export { GetRxController } from "./controller";
 export type { GetRxControllerFactory } from "./types";
 export { useGetPut, useGetFind, useOnObsChange } from "./hooks";
